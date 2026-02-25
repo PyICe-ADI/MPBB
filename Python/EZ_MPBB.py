e7c2ce1714cd6444d8ae31fb2cdfec7b3150e996
ANALOG\RMcFadde <russell.mcfadden@analog.com>
2026-02-25 13:09:32 -0500

    Reorder the arguments to match the class.

--- Python/EZ_MPBB.py
+++ Python/EZ_MPBB.py
@@ -7,7 +7,7 @@ from PyICe.lab_utils.banners import print_banner
 print_banner("", "Please enter the COM port number for your MPBB.", "", "It can be obtained from the Windows Device Manager.", "")
 comport = input("Enter COM port number, COM")
 channel_master = lab_core.channel_master()
-MPBB_board = MPBB(channel_master=channel_master, comport=f"COM{comport}", verbose=False)
+MPBB_board = MPBB(comport=f"COM{comport}", verbose=False, channel_master=channel_master)
 channel_master.add(MPBB_board)
 channel_master.write("MPBB_enable", "OFF") # Board is stateful
 channel_master.write("MPBB_wd_svc_time", 16000)
